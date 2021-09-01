ff8597db5541676e33cdffd503babec9781886fc
Matt Burton <matthewwburton@gmail.com>
2021-09-01 12:36:18 -0500

    changed fps controller

--- Portal Unity/Assets/Code/Portal.cs
+++ Portal Unity/Assets/Code/Portal.cs
@@ -3,42 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 //using StarterAssets;
 using UnityStandardAssets;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Portal : MonoBehaviour
 {
-    public Transform linkedPortal;
+    public Portal linkedPortal;
+    public bool isActive = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        TeleportObject(other.transform);
+        if (isActive)
+        {
+            isActive = false;
+            linkedPortal.isActive = false;
+            TeleportObject(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isActive = true;
     }
 
     void TeleportObject(Transform objectTransform)
     {
-        //objectTransform.SetPositionAndRotation(linkedPortal.transform.position, linkedPortal.transform.rotation);
-        //objectTransform.position = linkedPortal.position;
-        objectTransform.position = Vector3.zero;
-        print("hi");
+        objectTransform.position = linkedPortal.transform.position;
     }
-
-	void TeleportObjects(Transform objectTransform)
-	{
-		// cache player rotation to revert after teleport
-		float xRot = objectTransform.rotation.x;
-		float zRot = objectTransform.rotation.z;
-
-		// set the player's position and rotation to the other portal's
-		//TODO Actually teleport the player...
-		//objectTransform.position = linkedPortal.transform.position;
-		objectTransform.SetPositionAndRotation(linkedPortal.transform.position, linkedPortal.transform.rotation);
-
-		// Y rotation from portal
-		float yRot = objectTransform.eulerAngles.y;
-
-		// combine previously cached axes with new Y to get new rotation. Prevents flipping upside-down
-		objectTransform.eulerAngles = new Vector3(xRot, yRot, zRot);
-
-		// override FPSController's mouse look caching
-
-	}
 }
