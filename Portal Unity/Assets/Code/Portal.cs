d6f5759a4caabed69f199e7c38a986b5cd648779
Matt Burton <matthewwburton@gmail.com>
2021-09-08 10:09:47 -0500

    darn

--- Portal Unity/Assets/Code/Portal.cs
+++ Portal Unity/Assets/Code/Portal.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using StarterAssets;
-using UnityStandardAssets;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class Portal : MonoBehaviour
@@ -25,8 +23,34 @@ public class Portal : MonoBehaviour
         isActive = true;
     }
 
+    //void TeleportObject(Transform objectTransform)
+    //{
+    //    print(objectTransform.gameObject.name);
+    //    objectTransform.position = Vector3.zero;
+    //    //objectTransform.position = linkedPortal.transform.position;
+    //}
+
     void TeleportObject(Transform objectTransform)
     {
-        objectTransform.position = linkedPortal.transform.position;
+        // cache player rotation to revert after teleport
+        float xRot = objectTransform.rotation.x;
+        float zRot = objectTransform.rotation.z;
+
+        // set the player's position and rotation to the other portal's
+        //TODO Actually teleport the player...
+        //objectTransform.position = linkedPortal.transform.position;
+        objectTransform.SetPositionAndRotation(linkedPortal.transform.position, linkedPortal.transform.rotation);
+
+        // Y rotation from portal
+        float yRot = objectTransform.eulerAngles.y;
+
+        // combine previously cached axes with new Y to get new rotation. Prevents flipping upside-down
+        objectTransform.eulerAngles = new Vector3(xRot, yRot, zRot);
+
+        // override FPSController's mouse look caching
+        if (objectTransform.GetComponent<RigidbodyFirstPersonController>())
+        {
+            objectTransform.GetComponent<RigidbodyFirstPersonController>().MouseReset();
+        }
     }
 }
